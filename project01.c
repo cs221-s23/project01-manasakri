e0772575df4a07611913577a192f57219d9f681a
Manasa Krishnan <mkrishnan3@vlab02.cs.usfca.edu>
2023-02-14 21:52:25 -0800

    adding project01

--- project01.c
+++ project01.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -32,3 +31,82 @@ void sha256(char *dest, char *src) {
         dest += 2;
     }
 }
+char * change_leet(char* input_string);
+char * change_leet(char* input_string) {
+    char* changed_string =  malloc(strlen(input_string));
+    for (int i = 0 ; i < strlen(input_string); i++ ){
+        char c = input_string[i];
+        if (c == 'a'){
+            changed_string[i] = '@';
+        }
+        else if (c == 's') {
+            changed_string[i] = '$' ;
+        }
+        else if (c == 'o') {
+            changed_string[i] = '0' ;
+        }
+        else if (c == 't') {
+            changed_string[i] = '+' ;
+        }
+        else if (c == 'i') {
+            changed_string[i] = '!' ;
+        }
+        else if (c == 'h') {
+            changed_string[i] = '#' ;
+        }
+        else if (c == 'e') {
+            changed_string[i] = '3' ;
+        }
+        else {
+              changed_string[i] = c;
+               }
+    }
+    return changed_string;
+}
+
+char * change_plus_one(char* input_string);
+char * change_plus_one(char* input_string){
+    size_t len = strlen(input_string);
+    char *changed_string = malloc(len + 1 + 1);
+    strcpy(changed_string, input_string);
+
+    changed_string[len] = '1';
+    changed_string[len + 1] = '\0';
+    return changed_string ; 
+    }
+
+int main(int argc, char *argv[]) {
+ char * input_hash = argv[1] ;
+
+ char hashed_striing[DIG_STR_LEN] ;
+ char * string_leet;
+ char * plus_one;
+ char* temp_string;
+
+        for ( int i=0 ; i < 10000 ; i++ ) {
+            temp_string = passwords[i];
+            sha256(hashed_striing, temp_string);
+          //  printf("SHA for the entry %s is %s\n", temp_string, hashed_striing) ;
+            if ( strcmp( hashed_striing, input_hash) == 0 ) {
+                printf("%s\n", temp_string) ;
+                exit(0); 
+            }
+            else {
+                string_leet = change_leet(temp_string);
+                sha256(hashed_striing, string_leet);
+               if (strcmp(hashed_striing, input_hash) == 0) {
+                   printf("%s\n", string_leet);
+                   exit(0);
+               }
+               else {
+                   plus_one = change_plus_one(temp_string);
+                   sha256(hashed_striing, plus_one);
+                   if (strcmp(hashed_striing, input_hash) == 0) {
+                       printf("%s\n", plus_one);
+                       exit(0);
+                   }
+               }
+            }
+        }
+        printf("not found\n");
+    }
